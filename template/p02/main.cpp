64141b242ddd85bac2e798ba2f601170a67e1214
muumu <tielware@gmail.com>
2014-02-15 23:39:06 +0900

    added constexpr version of fib

--- template/p02/main.cpp
+++ template/p02/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "mytimer.h"
 
+typedef unsigned long long ull;
+
 int factorial_r(int n) {
     if (n <= 1) {
         return 1;
@@ -16,15 +18,15 @@ int factorial_loop(int n) {
     return result;
 }
 
-unsigned long long fib_r(int n) {
+ull fib_r(int n) {
     if (n <= 1) {
         return n;
     }
     return fib_r(n - 1) + fib_r(n - 2);
 }
 
-unsigned long long fib_mem(int n) {
-    static unsigned long long cache[100] = {0};
+ull fib_mem(int n) {
+    static ull cache[100] = {0};
     if (n <= 1) {
         return n;
     }
@@ -34,7 +36,7 @@ unsigned long long fib_mem(int n) {
     return cache[n] = fib_mem(n - 1) + fib_mem(n - 2);
 }
 
-int fib_loop(int n) {
+ull fib_loop(int n) {
     if (n <= 1) {
         return n;
     }
@@ -49,11 +51,25 @@ int fib_loop(int n) {
     return res;
 }
 
+ull fib_tail_opt(int n, int a = 1, int b = 0) {
+    if (n <= 1) {
+        return a;
+    }
+    return fib_tail_opt(n - 1, a + b, a);
+}
+
+constexpr ull fib_c(int n) {
+    return n <= 1 ? n : fib_c(n - 1) + fib_c(n - 2);
+}
+
+constexpr ull fib_c_opt(int n, int a = 1, int b = 0) {
+    return n <= 1 ? a : fib_c_opt(n - 1, a + b, a);
+}
 
 template <int N>
 struct fib {
     //enum {value = fib<N-1>::value + fib<N-2>::value};
-    static const unsigned long long value = fib<N - 1>::value + fib<N - 2>::value;
+    static const ull value = fib<N - 1>::value + fib<N - 2>::value;
 };
 template <>
 struct fib<0> {
@@ -63,6 +79,22 @@ template <>
 struct fib<1> {
     enum {value = 1};
 };
+
+template <int N, ull A = 1, ull B = 0>
+struct fib_t_opt {
+    //enum {value = fib<N-1>::value + fib<N-2>::value};
+    static const ull value = fib_t_opt<N - 1, A + B, A>::value;
+};
+template <ull A, ull B>
+struct fib_t_opt<0, A, B> {
+    static const ull value = A;
+};
+template <ull A, ull B>
+struct fib_t_opt<1, A, B> {
+    static const ull value = A;
+};
+
+
 
 template <template<int> class T, int N>
 struct do_n {
@@ -79,11 +111,12 @@ struct do_n<T, 0> {
 
 int main(int argc, char** argv) {
     MyTimer timer;
+    const int num = argc > 1 ? atoi(argv[1]) : 10;
     timer.start("factorial recursion");
-    std::cout << factorial_r(std::stoi(argv[1])) << std::endl;
+    std::cout << factorial_r(num) << std::endl;
     timer.stop();
     timer.start("factorial loop");
-    std::cout << factorial_loop(std::stoi(argv[1])) << std::endl;
+    std::cout << factorial_loop(num) << std::endl;
     timer.stop();
     timer.start("10");
     for (int i = 0; i < 10; ++i) {
@@ -106,6 +139,16 @@ int main(int argc, char** argv) {
         std::cout << fib_loop(i) << std::endl;
     }
     timer.stop();
-    do_n<fib, 40>::println();
+    timer.start("fib_tail_opt");
+    for (int i = 0; i < 40; ++i) {
+        std::cout << fib_tail_opt(i) << std::endl;
+    }
+    timer.stop();
+    timer.start("fib compile time constant");
+    //do_n<fib, 40>::println();
+    //std::cout << fib<50>::value << std::endl;
+    //std::cout << fib_t_opt<50>::value << std::endl;
+    std::cout << fib_c_opt(500) << std::endl;
+    timer.stop();
     timer.showResults();
 }
